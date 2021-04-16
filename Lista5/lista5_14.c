02c7490f0ae991854f7aa481107aff6719427523
DinossauroBebado <guipsilva32@gmail.com>
2021-04-15 21:17:34 -0300

    me mata com uma colher : D

--- Lista5/lista5_14.c
+++ Lista5/lista5_14.c
@@ -3,16 +3,25 @@
 //deve checar se o inteiro a está presente na matriz.
 
 
-int checa(int*M,int a){
-    int i,j;
-    if (!checa(int*M,int a)){
-        if(M[i][j]==a){
-            return 1;
-        }
-        else {
-            return 0;
+int checa(int*M,int a,int cont){
 
-        }
-        }
+if(*M==a)
+    return 1;
+else
+  if(cont==3){
+    return 0;
+  }
+    cont++;
+  return checa(M+1,a,cont);
+}
 
+int main(){
+int M[2][2] = {{1,2},{3,4}};
+int *p = &M[0][0];
+int i = 0;
+if(checa(p,i,0))
+    printf("o inteiro  esta presente na matriz\n");
+else{
+    printf("o inteiro NAO esta presente na matriz\n");
+}
 }
--- Lista5/lista5_15.c
+++ Lista5/lista5_15.c
@@ -2,6 +2,31 @@
 //strings de tamanho numStrings >= 0 e imprime o comprimento de cada string, um por linha.
 
  void imprimeTamString(int numStrings, char **strings){
+int i ;
+
+
+ printf("%s\n",strings);
+  printf("%s\n",strings+1);
+  printf("%s\n",strings+2);
+  printf("%s\n",strings+3);
+
+//for(i=0;i<numStrings;i++){
+//    printf("%s",**(strings+i));
+//
+//
+//}
+
+}
+
+
+
+
+ int main(){
+ int numStrings = 5;
+ char string[4][4] = {"maca","bana","abac","pera"};
+
+ imprimeTamString(numStrings,&string[0][0]);
+
 
 
 
