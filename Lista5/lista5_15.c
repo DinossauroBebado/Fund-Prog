0f5a25a9f610fdf8b28e6d7a1f0bf7646fe7a37d
DinossauroBebado <guipsilva32@gmail.com>
2021-04-16 14:00:24 -0300

    Amem StackOverflow salvador de todos nos

--- Lista5/lista5_15.c
+++ Lista5/lista5_15.c
@@ -2,19 +2,23 @@
 //strings de tamanho numStrings >= 0 e imprime o comprimento de cada string, um por linha.
 
  void imprimeTamString(int numStrings, char **strings){
-int i ;
+  int i,j,cont = 0;
+//printf("%c \n",*((*(strings+1))+2) );
+//   because you need to first de-reference to one of the actual string pointers and then you to
+//   de-reference that selected string pointer down to the desired character.
+//  (Note that I added extra parenthesis for clarity in the order of operations there).
 
+  for (i = 0; i <numStrings; i++){
+     for (j = 0; j <sizeof(strings); j++){
+      if(*((*(strings+i))+j)=='\0'){
+        printf("%s : %d \n",*(strings+i), cont);
+        cont = 0;
+        break;
+      }
+       cont++;
+      }
+  }
 
- printf("%s\n",strings);
-  printf("%s\n",strings+1);
-  printf("%s\n",strings+2);
-  printf("%s\n",strings+3);
-
-//for(i=0;i<numStrings;i++){
-//    printf("%s",**(strings+i));
-//
-//
-//}
 
 }
 
@@ -22,12 +26,9 @@ int i ;
 
 
  int main(){
- int numStrings = 5;
- char string[4][4] = {"maca","bana","abac","pera"};
 
- imprimeTamString(numStrings,&string[0][0]);
-
-
-
+ char *strings[4] = {"maca","abacaxi","banana","pera" };
+ int numStrings =4;
+ imprimeTamString(numStrings,strings);
 
  }
