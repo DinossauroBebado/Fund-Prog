488f58336b7062e9e6d144589aa0b83460ecb197
DinossauroBebado <guipsilva32@gmail.com>
2021-05-02 17:03:04 -0300

    Update lista6_8.c

--- Lista6/lista6_8.c
+++ Lista6/lista6_8.c
@@ -1,9 +1,7 @@
-
-//Faça um programa que receba a nota e o nome de uma quantidade de alunos
-//fornecida pelo usuário, calcule a média, quantidade de notas acima da média, e
-//quantidade de notas abaixo da média. (Obs. Utilize struct, alocação dinâmica de
-//memória e função com parâmetro por referência).
-
+/* 8. Faça um programa que receba a nota e o nome de uma quantidade de alunos
+fornecida pelo usuário, calcule a média, quantidade de notas acima da média, e
+quantidade de notas abaixo da média. (Obs. Utilize struct, alocação dinâmica de
+memória e função com parâmetro por referência). */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,43 +15,47 @@ typedef struct {
 float media(notas *not, int n ){
  int i;
  float sum= 0;
+ float d;
  for(i=0;i<n;i++){
    sum = sum + not[i].nota;
  }
- return sum/n ;
+ d = sum/n;
+ return d;
 }
 
 int main(){
-int n,i,maior,menor ;
+int n,i;
+float maior,menor ;
 maior = 0;
 menor = 0;
-float med = 6.0 ;
 
 printf("Informe o numero de alunos: ");
 scanf("%d", &n);
 
-notas *not = (notas*) malloc(n*sizeof(notas));
+notas * not = (notas *) malloc(n*sizeof(notas));
+
+
 
 for(i=0;i<n;i++){
     printf("--------%d---------\n",i);
     printf("Nome :");
-    fflush(stdin);
+    setbuf(stdin, NULL);
     gets(not[i].nome);
 
     printf("Nota : ");
     scanf("%f",&not[i].nota);
 
-    if(not[i].nota>=med){
+    if(not[i].nota>=media(not,n)){
         maior ++ ;
         }
 }
-
-menor = n-maior;
+    menor = n - maior;
 printf("------------------------\n");
-printf("    Maior nota : %.d\n    Menor nota : %.d\n    Media : %.2f\n",
+printf("    Acima : %.f\n    Abaixo: %.f\n    Media : %.2f\n",
        maior,
        menor,
        media(not,n));
 printf("------------------------\n");
 
+ return 0;
 }
